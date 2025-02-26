ad11f39bd91091a9193353d7e705fca9ec9e3184
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2025-02-26 18:54:29 +0100

    Edit SmartDoorLock

---
+++ tdd-exercise/src/main/java/model/SmartDoorLock.java
@@ -0,0 +1,71 @@
+package model;
+
+public class SmartDoorLock implements tdd.SmartDoorLock {
+
+    private static final int MAX_ATTEMPTS = 3;
+    private static final int DEFAULT_PIN = -1;
+
+    private boolean doorLockedState;
+    private boolean doorBlockedState;
+    private int pin;
+    private int failedAttempts;
+
+    public SmartDoorLock() {
+        this.doorLockedState = false;
+    }
+
+    @Override
+    public void setPin(int pin) {
+        if(!this.doorLockedState && !this.doorBlockedState) {
+            this.pin = pin;
+        }
+    }
+
+    @Override
+    public void unlock(int pin) {
+        if (this.pin == pin) {
+            this.doorLockedState = false;
+            this.failedAttempts = 0;
+        } else {
+            this.failedAttempts++;
+            if (this.failedAttempts >= MAX_ATTEMPTS) {
+                this.doorBlockedState = true;
+            }
+        }
+    }
+
+    @Override
+    public void lock() {
+        if(this.pin != DEFAULT_PIN) {
+            this.doorLockedState = true;
+        } else throw new IllegalStateException("PIN is not set");
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.doorLockedState;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return this.doorBlockedState;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return MAX_ATTEMPTS;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return this.failedAttempts;
+    }
+
+    @Override
+    public void reset() {
+        this.pin = DEFAULT_PIN;
+        this.failedAttempts = 0;
+        this.doorBlockedState = false;
+        this.doorLockedState = false;
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,74 @@
 package tdd;
 
+import model.SmartDoorLock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    public static final int TEST_PIN = 1234;
+    public static final int WRONG_TEST_PIN = 4321;
+
+    private tdd.SmartDoorLock smartDoorLock;
+
+    @BeforeEach
+    void beforeEach(){
+        smartDoorLock = new SmartDoorLock();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testDoorIsInitiallyUnlocked() {
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testSetPin() {
+        smartDoorLock.setPin(TEST_PIN);
+        smartDoorLock.unlock(TEST_PIN);
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testDoorIsLocked() {
+        smartDoorLock.lock();
+        assertTrue(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testDoorIsUnlocked() {
+        smartDoorLock.unlock(TEST_PIN);
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testUnlockWithWrongPin() {
+        smartDoorLock.lock();
+        smartDoorLock.unlock(WRONG_TEST_PIN);
+        assertTrue(smartDoorLock.isLocked());
+        assertNotEquals(0, smartDoorLock.getFailedAttempts());
+    }
+
+    @Test
+    public void testBlockTheDoor() {
+        smartDoorLock.lock();
+        int maxAttempts = smartDoorLock.getMaxAttempts();
+        for (int i = 0; i < maxAttempts; i++) {
+            smartDoorLock.unlock(WRONG_TEST_PIN);
+        }
+        assertTrue(smartDoorLock.isBlocked());
+        assertEquals(maxAttempts, smartDoorLock.getFailedAttempts());
+    }
+
+    @Test
+    public void testResetDoor() {
+        smartDoorLock.reset();
+
+        assertFalse(smartDoorLock.isLocked());
+        assertEquals(0, smartDoorLock.getFailedAttempts());
+        smartDoorLock.unlock(TEST_PIN);
+        assertNotEquals(0, smartDoorLock.getFailedAttempts());
+        assertFalse(smartDoorLock.isBlocked());
     }
 }
